453559fb798815aff613d4093db5ffcb1028e069
Wisimer <wxp19921015@icloud.com>
2025-08-04 22:16:26 +0800

    feat:热门接口

---
+++ app/api/hot-avatars/route.ts
@@ -0,0 +1,34 @@
+import { type NextRequest, NextResponse } from "next/server"
+import { formatQuery, signV4Request } from '@/app/api/jimeng';
+import { supabase } from "@/lib/supabase";
+
+const VOLC_ACCESSKEY = process.env.VOLC_ACCESSKEY
+const VOLC_SECRETKEY = process.env.VOLC_SECRETKEY
+const SERVICE = "cv";
+export async function POST(request: NextRequest) {
+  try {
+
+    const { pageNum, pageSize } = await request.json()
+
+    const { data, error } = await supabase
+      .from("sport_avatar")
+      .select("*")
+      .range((pageNum - 1) * pageSize, pageNum * pageSize)
+      .order("download_count", { ascending: false })
+    if (error) {
+      return NextResponse.json({
+        success: false,
+        message: "获取失败",
+      })
+    }
+
+    return NextResponse.json({
+      success: true,
+      data: data,
+      message: "海报生成成功",
+    })
+  } catch (error) {
+    console.error("生成海报失败:", error)
+    return NextResponse.json({ success: false, message: "生成失败，请重试" }, { status: 500 })
+  }
+}
